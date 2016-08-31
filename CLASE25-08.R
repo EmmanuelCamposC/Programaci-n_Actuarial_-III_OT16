0d71540d2e026c76a5823520b50e95034d74e605
Emmanuel C.C <comoseanoimpor@gmail.com>
2016-08-31 09:43:50 -0500

    Se agregó una primer baso de datos
    se dercargó de yahoo finance la base de datos de precios de S&P y se uso
    la función read.csv y read.table

--- CLASE25-08.R
+++ CLASE25-08.R
@@ -100,9 +100,62 @@ m1
 m2 <- cbind(m,x)
 m2
   
-m3 <- rbind(m1,y)
-m3
+rbind(m1,y)
+cbind(m2,y)
 
-m4 <- cbind(m2,y)
-m4  
-  
+#factores 
+x <- factor(c("si","no","si","si","no","si","no"))
+x
+table(x)
+unclass(x)
+x <- factor(c("si","no","si","si","no","si","no"),levels = c("si","no")) #unclass(x)
+
+x <- factor(c("azul","azul","rojo","azul","amarillo ","verde","azul"))
+x
+table(x)
+
+#Valores faltantes 
+x <- c(1,2,NaN,10,3)
+is.na(x)
+is.nan(x)
+
+#Data Frames
+x <- data.frame(Erick = 1:4, Lori = c(T,T,F,F))
+row.names(x) <- c("Primero","Segundo","Tercero","Cuarto")
+x
+nrow(x)
+ncol(x)
+attributes(x)
+names(x) <- c("Yarely","Karen")
+x
+
+
+
+
+
+# Los nombres no son exclusivos de los data frames
+x <- 1:3
+names(x) #NULL
+names(x) <- c("Hugo","Paco","Luis")
+x
+
+x <- list(a=1:10,b=100:91,c=51:60)
+x
+names(x) <- c("seq1","SEQ2","sEq3")
+x
+
+
+m <- matrix(1:4,2,2)
+m 
+attributes(m)
+dimnames(m) <- list(c("fil1","fil2"),c("col1","col2"))
+m
+
+#lectura de datos 
+getwd()
+setwd("~/GitHub/Programaci-n_Actuarial_-III_OT16")
+data <- read.csv("Datos_S&P.csv")
+data <- read.table("Datos_S&P.csv",T,",")
+data
+
+
