829dae09cea5dcc7861cff4cc0f3c8fd1a789df9
Emmanuel C.C <comoseanoimpor@gmail.com>
2016-09-21 22:06:09 -0500

    Caso 1
    Correccion

--- Caso_No.1/completos.R
+++ Caso_No.1/completos.R
@@ -1,16 +1,22 @@
-c(TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, TRUE, FALSE, 
-FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, 
-TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, TRUE, FALSE, 
-TRUE, TRUE, FALSE, FALSE, TRUE, FALSE, FALSE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, 
-FALSE, FALSE, FALSE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, FALSE, 
-TRUE, TRUE, TRUE, FALSE, FALSE, TRUE, TRUE, TRUE, FALSE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, TRUE, 
-FALSE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, 
-TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, TRUE, FALSE, 
-TRUE, TRUE, TRUE)
+function (directorio, id = 1:332) 
+{
+    setwd(directorio)
+    llenado <- vector("numeric")
+    for (k in id) {
+        if (k < 10) {
+            archivo <- read.csv(paste("00", k, ".csv", sep = ""))
+        }
+        if (k >= 10 && k < 100) {
+            archivo <- read.csv(paste("0", k, ".csv", sep = ""))
+        }
+        if (k >= 100) {
+            archivo <- read.csv(paste(k, ".csv", sep = ""))
+        }
+        comp <- complete.cases(archivo)
+        full <- archivo[comp, 2:3]
+        llenar <- nrow(full)
+        llenado <- c(llenado, llenar)
+    }
+    daframe <- data.frame(id = id, nobs = llenado)
+    daframe
+}
